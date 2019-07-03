beb82b2079cae56295ab36d8939814b5d0807e49
Charlie Taylan <charlietaylan@gmail.com>
2019-07-03 22:41:05 +0800

    Personal Website 1st Commit

---
+++ PersonalWebsite.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Charlie M. Taylan</title>
+	<meta name="viewport"  charset="utf-8" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="plugins/css/bootstrap.min.css">
+</head>
+<body>
+	<section id="personalweb">
+		<section id="home">
+			<div>
+				<h2>Hi!</h2>
+				<h3>I am Charlie Moring Taylan</h3>
+				<h3>BS Information Tachnology</h3>
+				<button><h3>Hire Me</h3></button>
+			</div>
+		</section>
+	</section>
+</body>
+</html>
